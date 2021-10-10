6416e3a9bb55a4a93b521c388e89b54141f34837
Linkan42 <sali20jt@student.ju.se>
2021-10-10 12:50:53 +0200

    task 4, excl. loop, done

--- lab3_Linus.hs
+++ lab3_Linus.hs
@@ -1,7 +1,7 @@
 type Binding = (Variable,Value)
 type State = [Binding]
 type Variable = String
-data Value = Intval Integer deriving (Eq, Ord,Show)
+data Value = Intval Integer deriving (Eq,Ord,Show)
 
 --Task 1
 get :: Variable -> State -> Value   --tested
@@ -10,7 +10,7 @@ get var (x:xs) = if var == fst x then snd x else get var xs
 onion :: Variable -> Value -> State -> State    --tested
 onion var val st = (var,val) : [x | x <- st, var /= fst x]
 
-data Expression = Var Variable | Lit Value | Aop Op Expression Expression deriving (Eq, Ord, Show) -- Aritmetic operators
+data Expression = Var Variable | Lit Value | Aop Op Expression Expression deriving (Eq,Ord,Show) -- Aritmetic operators
 
 type Op = String
 
@@ -26,30 +26,33 @@ apply op (Intval x) (Intval y)
     | op == "*" = Intval $ x * y
     | op == "/" = Intval $ round $ fromIntegral x / fromIntegral y
 
-data Bexpression = Blit Bvalue | Bop Op Bexpression Bexpression | Rop Op Expression Expression deriving (Eq, Ord,Show)
+data Bexpression = Blit Bvalue | Bop Op Bexpression Bexpression | Rop Op Expression Expression deriving (Eq,Ord,Show)
                                         -- Boolean operators               -- Relational operators
-data Bvalue = Boolval Bool deriving (Eq, Ord,Show)
+data Bvalue = Boolval Bool deriving (Eq,Ord,Show)
 
 --Task 3
-beval :: Bexpression -> State -> Bvalue
+beval :: Bexpression -> State -> Bvalue --tested
 beval (Blit b) _ = b
 beval (Bop op b1 b2) st = bapply op (beval b1 st) (beval b2 st)
---beval (Rop op e1 e2) st = rapply op 
+beval (Rop op e1 e2) st = rapply op (eval e1 st) (eval e2 st)
 
-bapply :: Op -> Bvalue -> Bvalue -> Bvalue
-bapply op b1@(Boolval x) b2@(Boolval y)
-    | op == "&&" = 
-    | op == "||" = 
+bapply :: Op -> Bvalue -> Bvalue -> Bvalue  --tested
+bapply op (Boolval x) (Boolval y)
+    | op == "&&" && x == True && y == True = Boolval True 
+    | op == "||" && (x == True || y == True) = Boolval False
+    | otherwise = Boolval False 
 
---rapply :: Op -> 
---rapply
+rapply :: Op -> Value -> Value -> Bvalue    --tested
+rapply op (Intval x) (Intval y)
+    | op == "<" && x < y = Boolval True 
+    | op == "<=" && x <= y = Boolval True
+    | op == "==" && x == y = Boolval True 
+    | op == "!=" && x /= y = Boolval True 
+    | op == ">=" && x >= y = Boolval True 
+    | op == ">" && x > y = Boolval True
+    | otherwise = Boolval False
 
-data Statement = Skip |
- Assignment Target Source |
- Block Blocktype | 
- Loop Test Body |
- Conditional Test Thenbranch Elsebranch
- deriving (Show)
+data Statement = Skip | Assignment Target Source | Block Blocktype | Loop Test Body | Conditional Test Thenbranch Elsebranch deriving Show
 
 type Target = Variable
 type Source = Expression
@@ -58,22 +61,29 @@ type Body = Statement
 type Thenbranch = Statement
 type Elsebranch = Statement
 
-data Blocktype = Nil | 
- Nonnil Statement Blocktype
- deriving (Show)
+m :: Statement -> State -> State
+m (Skip) state = state
+m (Assignment target source) state = onion target (eval source state) state
+m (Loop test body) state    --fix
+    | beval test state == Boolval True = m body state
+    | otherwise = state --skip
+m (Conditional test thenbranch elsebranch) state
+    | beval test state == Boolval True = m thenbranch state
+    | otherwise = m elsebranch state
 
+data Blocktype = Nil | Nonnil Statement Blocktype deriving (Show)
 
 s1 :: State
 s1=[("x",(Intval 1)) ,("y",(Intval 5))]
 
---p0::Statement -- An assignment
---p0 = (Assignment "x" (Aop "+" (Var "x") (Lit ( Intval 1))))
+p0::Statement -- An assignment
+p0 = (Assignment "x" (Aop "+" (Var "x") (Lit (Intval 1))))
 
---p1::Statement -- A loop. 
---p1=(Loop (Rop "<" (Var "x") (Lit(Intval 10))) (Assignment "x" (Aop "+" (Var "x") (Lit(Intval 1)))))
+p1::Statement -- A loop. 
+p1=(Loop (Rop "<" (Var "x") (Lit(Intval 10))) (Assignment "x" (Aop "+" (Var "x") (Lit(Intval 1)))))
 
---p2::Statement -- An IF-statement.
---p2=(Conditional (Rop ">" (Var "x") (Var "y")) (Assignment (Var "x") (Var "y")) (Assignment "x" (Aop "+" (Var "x") (Var "y"))))
+p2::Statement -- An IF-statement.
+p2=(Conditional (Rop ">" (Var "x") (Var "y")) (Assignment "x" (Var "y")) (Assignment "x" (Aop "+" (Var "x") (Var "y"))))
 
 --p3::Statement -- A Block (i.e. program) without instructions
 --p3=(Block Nil)
