29246ae3054f047ca0c0ae966008c40ec93aa14d
Linkan42 <sali20jt@student.ju.se>
2021-10-12 12:26:20 +0200

    task 4 except loop done

--- lab3_Linus.hs
+++ lab3_Linus.hs
@@ -3,7 +3,6 @@ type State = [Binding]
 type Variable = String
 data Value = Intval Integer deriving (Eq,Ord,Show)
 
---Task 1
 get :: Variable -> State -> Value   --tested
 get var (x:xs) = if var == fst x then snd x else get var xs
 
@@ -30,7 +29,6 @@ data Bexpression = Blit Bvalue | Bop Op Bexpression Bexpression | Rop Op Express
                                         -- Boolean operators               -- Relational operators
 data Bvalue = Boolval Bool deriving (Eq,Ord,Show)
 
---Task 3
 beval :: Bexpression -> State -> Bvalue --tested
 beval (Blit b) _ = b
 beval (Bop op b1 b2) st = bapply op (beval b1 st) (beval b2 st)
@@ -39,7 +37,7 @@ beval (Rop op e1 e2) st = rapply op (eval e1 st) (eval e2 st)
 bapply :: Op -> Bvalue -> Bvalue -> Bvalue  --tested
 bapply op (Boolval x) (Boolval y)
     | op == "&&" && x == True && y == True = Boolval True 
-    | op == "||" && (x == True || y == True) = Boolval False
+    | op == "||" && (x == True || y == True) = Boolval True
     | otherwise = Boolval False 
 
 rapply :: Op -> Value -> Value -> Bvalue    --tested
@@ -65,8 +63,8 @@ m :: Statement -> State -> State
 m (Skip) state = state
 m (Assignment target source) state = onion target (eval source state) state
 m (Loop test body) state    --fix
-    | beval test state == Boolval True = m body state
-    | otherwise = state --skip
+    | beval test state == Boolval True = 
+    | otherwise = state --i.e. skip
 m (Conditional test thenbranch elsebranch) state
     | beval test state == Boolval True = m thenbranch state
     | otherwise = m elsebranch state
@@ -74,22 +72,22 @@ m (Conditional test thenbranch elsebranch) state
 data Blocktype = Nil | Nonnil Statement Blocktype deriving (Show)
 
 s1 :: State
-s1=[("x",(Intval 1)) ,("y",(Intval 5))]
+s1=[("x",(Intval 1)),("y",(Intval 5))]
 
-p0::Statement -- An assignment
+p0 :: Statement -- An assignment
 p0 = (Assignment "x" (Aop "+" (Var "x") (Lit (Intval 1))))
 
-p1::Statement -- A loop. 
-p1=(Loop (Rop "<" (Var "x") (Lit(Intval 10))) (Assignment "x" (Aop "+" (Var "x") (Lit(Intval 1)))))
+p1 :: Statement -- A loop
+p1 = (Loop (Rop "<" (Var "x") (Lit(Intval 10))) (Assignment "x" (Aop "+" (Var "x") (Lit(Intval 1)))))
 
-p2::Statement -- An IF-statement.
-p2=(Conditional (Rop ">" (Var "x") (Var "y")) (Assignment "x" (Var "y")) (Assignment "x" (Aop "+" (Var "x") (Var "y"))))
+p2 :: Statement -- An IF-statement
+p2 = (Conditional (Rop ">" (Var "x") (Var "y")) (Assignment "x" (Var "y")) (Assignment "x" (Aop "+" (Var "x") (Var "y"))))
 
---p3::Statement -- A Block (i.e. program) without instructions
---p3=(Block Nil)
+--p3 :: Statement -- A Block (i.e. program) without instructions
+--p3 = (Block Nil)
 
---p4::Statement -- A Block (i.e. program) with instructions
---p4=(Block (Nonnil p2 (Nonnil p1 Nil)))
+--p4 :: Statement -- A Block (i.e. program) with instructions
+--p4 = (Block (Nonnil p2 (Nonnil p1 Nil)))
 
---run::Statement->State 
+--run :: Statement->State 
 --run program = m program s1
