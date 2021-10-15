69e447c0d75b88aaa1e2767dad5fd79e5b14c449
Linkan42 <sali20jt@student.ju.se>
2021-10-15 14:01:33 +0200

    fibonacci example almost done, needs fixing

--- lab3_Linus.hs
+++ lab3_Linus.hs
@@ -1,16 +1,25 @@
+data Value = Intval Integer deriving (Eq,Ord,Show)
+data Expression = Var Variable | Lit Value | Aop Op Expression Expression deriving (Eq,Ord,Show)
+data Bvalue = Boolval Bool deriving (Eq,Ord,Show)
+data Statement = Skip | Assignment Target Source | Block Blocktype | Loop Test Body | Conditional Test Thenbranch Elsebranch deriving Show
+data Bexpression = Blit Bvalue | Bop Op Bexpression Bexpression | Rop Op Expression Expression deriving (Eq,Ord,Show)
+data Blocktype = Nil | Nonnil Statement Blocktype deriving (Show)
 type Binding = (Variable,Value)
 type State = [Binding]
 type Variable = String
-data Value = Intval Integer deriving (Eq,Ord,Show)
+type Op = String
+type Target = Variable
+type Source = Expression
+type Test = Bexpression
+type Body = Statement
+type Thenbranch = Statement
+type Elsebranch = Statement
 
 get :: Variable -> State -> Value
 get var (x:xs) = if var == fst x then snd x else get var xs
 
 onion :: Variable -> Value -> State -> State
 onion var val st = (var,val) : [x | x <- st, var /= fst x]
-
-data Expression = Var Variable | Lit Value | Aop Op Expression Expression deriving (Eq,Ord,Show)
-type Op = String
 
 eval:: Expression -> State -> Value
 eval (Var v) state = get v state
@@ -22,9 +31,6 @@ apply "+" (Intval x) (Intval y) = Intval $ x + y
 apply "-" (Intval x) (Intval y) = Intval $ x - y
 apply "*" (Intval x) (Intval y) = Intval $ x * y
 apply "/" (Intval x) (Intval y) = Intval $ round $ fromIntegral x / fromIntegral y
-
-data Bexpression = Blit Bvalue | Bop Op Bexpression Bexpression | Rop Op Expression Expression deriving (Eq,Ord,Show)
-data Bvalue = Boolval Bool deriving (Eq,Ord,Show)
 
 beval :: Bexpression -> State -> Bvalue
 beval (Blit b) _ = b
@@ -43,14 +49,6 @@ rapply "!=" (Intval x) (Intval y) = Boolval $ x /= y
 rapply ">=" (Intval x) (Intval y) = Boolval $ x >= y
 rapply ">" (Intval x) (Intval y) = Boolval $ x > y
 
-data Statement = Skip | Assignment Target Source | Block Blocktype | Loop Test Body | Conditional Test Thenbranch Elsebranch deriving Show
-type Target = Variable
-type Source = Expression
-type Test = Bexpression
-type Body = Statement
-type Thenbranch = Statement
-type Elsebranch = Statement
-
 m :: Statement -> State -> State
 m (Skip) state = state
 m (Assignment target source) state = onion target (eval source state) state
@@ -58,8 +56,6 @@ m (Loop test body) state = if beval test state == Boolval True then m (Loop test
 m (Conditional test thenbranch elsebranch) state = if beval test state == Boolval True then m thenbranch state else m elsebranch state
 m (Block Nil) state = state
 m (Block (Nonnil statement nextBlock)) state = m (Block nextBlock) (m statement state)
-
-data Blocktype = Nil | Nonnil Statement Blocktype deriving (Show)
 
 s1 :: State
 s1=[("x",(Intval 1)),("y",(Intval 5))]
@@ -84,3 +80,24 @@ pTest = Block (Nonnil p1 (Nonnil p0 (Nonnil p0 Nil)))
 
 run :: Statement -> State 
 run program = m program s1
+
+-- Example: generate infinite list of Fibonacci numbers
+runFib :: State
+runFib = m fib sFib
+
+sFib :: State
+sFib = [("counter",(Intval 3)),("Intval 2",(Intval 1)),("Intval 1",(Intval 1))]
+
+fib :: Statement
+fib = Loop (Blit (Boolval True)) (Block fibBlock)
+
+fibBlock :: Blocktype
+fibBlock = Nonnil calcNewVal $ Nonnil counterIncrease $ Nil
+
+calcNewVal :: Statement
+calcNewVal = Assignment (show (get "counter" sFib)) $ Aop "+" v1 v2 --create new Var with value v1 + v2
+    where v1 = Lit $ snd $ sFib !! 2    --2nd to last value
+          v2 = Lit $ snd $ sFib !! 1    --last value
+                                        --sFib !! 0 is counter
+counterIncrease :: Statement
+counterIncrease = Assignment "counter" $ Aop "+" (Var "counter") $ Lit $ Intval 1   --increase "counter" with 1
